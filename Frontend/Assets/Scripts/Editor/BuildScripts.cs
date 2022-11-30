b77717ec8d093f8fe90679ae3a5f8a0666cdbfc4
Chris Janidlo <christopherjanidlo@gmail.com>
2022-11-29 20:14:35 -0600

    Fix Windows builds to always run
    Previously, Windows builds would successfully run immediately after
    "Build and Run" in Unity, but would fail to load KinAI if they were run
    in any other context (ie by running the built exe in Windows Explorer,
    whether in the original build location or after moving the build folder
    elsewhere). By copying KinAI.dll to the build folder, `kernel32` can
    always find it when searching with `LoadLibrary`.

--- Frontend/Assets/Scripts/Editor/BuildScripts.cs
+++ Frontend/Assets/Scripts/Editor/BuildScripts.cs
@@ -1,8 +1,7 @@
-using System;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
-using System.IO;
 
 namespace Editor
 {
@@ -11,14 +10,44 @@ namespace Editor
         [PostProcessBuild]
         public static void OnPostProcessBuild(BuildTarget target, string pathToBuiltProject)
         {
-            if (target != BuildTarget.StandaloneOSX) return;
+            // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+            switch (target)
+            {
+                case BuildTarget.StandaloneWindows:
+                case BuildTarget.StandaloneWindows64:
+                    PostBuildWindows(pathToBuiltProject);
+                    break;
+                case BuildTarget.StandaloneOSX:
+                    PostBuildMac(pathToBuiltProject);
+                    break;
+                default:
+                    Debug.Log($"Unhandled {nameof(BuildTarget)}: {target}. No post-processing will occur");
+                    break;
+            }
+        }
 
+        private static void PostBuildWindows(string pathToBuiltProject)
+        {
+            const string filename = "KinAI.dll";
+
+            var projectDirectory = Directory.GetParent(Application.dataPath)!.FullName;
+            var buildDirectory = Directory.GetParent(pathToBuiltProject)!.FullName;
+
+            var sourcePath = Path.Combine(projectDirectory, filename);
+            var destPath = Path.Combine(buildDirectory, filename);
+
+            Debug.Log($"Copying {sourcePath} to {destPath}");
+            File.Copy(sourcePath, destPath, true);
+        }
+
+        private static void PostBuildMac(string pathToBuiltProject)
+        {
             const string filename = "KinAI.bundle";
-            
+
             var projectDirectory = Directory.GetParent(Application.dataPath)!.FullName;
             var sourcePath = Path.Combine(projectDirectory, filename);
             var destPath = Path.Combine(pathToBuiltProject, filename);
-            
+
             Debug.Log($"Copying {sourcePath} to {destPath}");
             File.Copy(sourcePath, destPath, true);
         }
