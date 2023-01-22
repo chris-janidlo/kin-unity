cff478135dfcef442c73654694d81e95176744ff
Chris Janidlo <christopherjanidlo@gmail.com>
2023-01-22 12:38:33 -0600

    Make make make more sense

---
+++ AI/platform-make-directives/mac.mk
@@ -0,0 +1,5 @@
+AI_SERVER_SRC=target/release/ai_server
+AI_SERVER_DEST=../Frontend/ai_server
+
+BOOTSTRAPPER_SRC=target/release/libbootstrapper.dylib
+BOOTSTRAPPER_DEST=../Frontend/Assets/Plugins/bootstrapper.dylib
---
+++ AI/platform-make-directives/windows.mk
@@ -0,0 +1,5 @@
+AI_SERVER_SRC=target/release/ai_server.exe
+AI_SERVER_DEST=../Frontend/ai_server
+
+BOOTSTRAPPER_SRC=target/release/bootstrapper.dll
+BOOTSTRAPPER_DEST=../Frontend/Assets/Plugins/bootstrapper.dll
