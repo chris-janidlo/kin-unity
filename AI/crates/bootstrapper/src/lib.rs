2884e5b9f4f15929f999dcb6625409dc75cff242
Chris Janidlo <christopherjanidlo@gmail.com>
2023-01-22 23:20:25 -0600

    Improve error handling in bootstrapper
    Now, rather than panicking, we do the following:
    1. dump the error to a file in the same directory as the game exe
    2. return -1 to signal to C# that an exception occurred

--- AI/crates/bootstrapper/src/lib.rs
+++ AI/crates/bootstrapper/src/lib.rs
@@ -3,14 +3,16 @@ use std::os::windows::process::CommandExt;
 use std::{
     cell::RefCell,
     collections::HashMap,
-    env::current_exe,
-    fs,
-    io::Read,
+    env::{current_dir, current_exe},
+    fs::{read_dir, File},
+    io::{Read, Write},
     path::PathBuf,
     process::{Child, Command, Stdio},
+    result::Result::{Err, Ok},
+    time::{SystemTime, UNIX_EPOCH},
 };
 
-// TODO: better error handling - log some human readable info in a file somwhere, and only then panic
+use anyhow::*;
 
 thread_local! {
     static PROC_MAP: RefCell<HashMap<u32, Child>> = RefCell::new(Default::default());
@@ -18,8 +20,62 @@ thread_local! {
 
 /// Launches an `ai_server` process, returning its PID.
 #[no_mangle]
-pub extern "C" fn open_server() -> u32 {
-    let mut command = Command::new(get_server_path());
+pub extern "C" fn open_server() -> i64 {
+    match open_server_impl() {
+        Ok(o) => o as i64,
+        Err(e) => dump(e),
+    }
+}
+
+/// Retrieves the TCP port from an `ai_server` process, as delimited by PID.
+// TODO: allow consumers to call this more than once for the same PID without crashing?
+#[no_mangle]
+pub extern "C" fn get_tcp_port(pid: u32) -> i64 {
+    match get_tcp_port_impl(pid) {
+        Ok(o) => o as i64,
+        Err(e) => dump(e),
+    }
+}
+
+/// Closes a given `ai_server` process by PID.
+#[no_mangle]
+pub extern "C" fn close_server(pid: u32) -> i64 {
+    match close_server_impl(pid) {
+        Ok(_) => 1,
+        Err(e) => dump(e),
+    }
+}
+
+/// Close every open `ai_server` process that was spawned from this thread.
+#[no_mangle]
+pub extern "C" fn close_all() -> i64 {
+    match close_all_impl() {
+        Ok(_) => 1,
+        Err(e) => dump(e),
+    }
+}
+
+fn dump(err: Error) -> i64 {
+    // since we're in an error handling function, give up without complaint if anything goes wrong
+    if let Ok(mut path) = current_dir() {
+        if let Ok(duration) = SystemTime::now().duration_since(UNIX_EPOCH) {
+            let timestamp = duration.as_secs();
+            let filename = format!("ai_bootstrap_crash_{timestamp}.log");
+
+            path.push(filename);
+
+            if let Ok(mut file) = File::create(path) {
+                // use _ to ignore result
+                let _ = write!(file, "{err}");
+            }
+        }
+    }
+
+    -1
+}
+
+fn open_server_impl() -> Result<u32> {
+    let mut command = Command::new(get_server_path()?);
     command.stdout(Stdio::piped());
 
     #[cfg(windows)]
@@ -28,54 +84,49 @@ pub extern "C" fn open_server() -> u32 {
         command.creation_flags(CREATE_NO_WINDOW);
     }
 
-    let server_proc = command.spawn().expect("should be able to launch process");
+    let server_proc = command
+        .spawn()
+        .context("should be able to launch ai_server")?;
 
     let pid = server_proc.id();
 
     PROC_MAP.with(|map| map.borrow_mut().insert(pid, server_proc));
 
-    pid
+    Ok(pid)
 }
 
-/// Retrieves the TCP port from an `ai_server` process, as delimited by PID.
-// TODO: allow consumers to call this more than once for the same PID without crashing?
-#[no_mangle]
-pub extern "C" fn get_tcp_port(pid: u32) -> u32 {
-    let mut port: u32 = 0;
-
+fn get_tcp_port_impl(pid: u32) -> Result<u32> {
     PROC_MAP.with(|map| {
         if let Some(proc) = map.borrow_mut().get_mut(&pid) {
             let mut stream = proc
                 .stdout
                 .take()
-                .expect("stdout should be set up and readable");
+                .context("stdout should be set up and readable")?;
 
-            port = read_port_number(&mut stream);
+            read_port_number(&mut stream)
         } else {
-            panic!("PID should be valid");
+            Err(anyhow!("PID should be valid"))
         }
-    });
-
-    port
+    })
 }
 
-/// Closes a given `ai_server` process by PID.
-#[no_mangle]
-pub extern "C" fn close_server(pid: u32) {
+fn close_server_impl(pid: u32) -> Result<()> {
     PROC_MAP.with(|map| {
         if let Some(mut proc) = map.borrow_mut().remove(&pid) {
-            proc.kill().expect("should be able to kill this process");
+            proc.kill().context("should be able to kill this process")
+        } else {
+            Err(anyhow!("PID {pid} is not known to this thread"))
         }
-    });
+    })
 }
 
-/// Close every open `ai_server` process that was spawned from this thread.
-#[no_mangle]
-pub extern "C" fn close_all() {
+fn close_all_impl() -> Result<()> {
     PROC_MAP.with(|map| {
         for (_, mut proc) in map.borrow_mut().drain() {
-            proc.kill().expect("should be able to kill this process");
+            proc.kill().context("should be able to kill this process")?;
         }
+
+        Ok(())
     })
 }
 
@@ -85,39 +136,36 @@ pub extern "C" fn close_all() {
 ///
 /// * `stream` - Any readable stream that has a port number. Is expected to have exactly
 ///   5 bytes of data to read, with left-padded 0s as necessary.
-fn read_port_number(stream: &mut impl Read) -> u32 {
+fn read_port_number(stream: &mut impl Read) -> Result<u32> {
     let mut buf = [0; 5];
 
     stream
         .read_exact(&mut buf)
-        .expect("should be able to read from stream");
+        .context("should be able to read from stream")?;
 
     std::str::from_utf8(&buf)
-        .expect("stream should be utf8 encoded")
+        .context("stream should be utf8 encoded")?
         .parse::<u32>()
-        .expect("message should be in integer form")
+        .context("message should be in integer form")
 }
 
-fn get_server_path() -> PathBuf {
-    let finding = fs::read_dir("./")
-        .expect("should be able to read current directory")
-        .find_map(|dir_entry| {
-            let de = dir_entry.expect("should be able to inspect dir_entry");
+fn get_server_path() -> Result<PathBuf> {
+    let paths = read_dir("./").context("should be able to read current directory")?;
 
-            if de.file_name() == "ai_server" {
-                Some(de.path())
-            } else {
-                None
-            }
-        });
+    for possible_entry in paths {
+        let entry =
+            possible_entry.context("intermittent IO error while reading current directory")?;
 
-    if let Some(path) = finding {
-        path
-    } else if cfg!(macos) {
-        let mut path = current_exe().expect("should be able to get running macOS app");
+        if entry.file_name() == "ai_server" {
+            return Ok(entry.path());
+        }
+    }
+
+    if cfg!(macos) {
+        let mut path = current_exe().context("should be able to get running macOS app")?;
         path.push("ai_server");
-        path
+        Ok(path)
     } else {
-        panic!("can't find ai_server")
+        Err(anyhow!("can't find ai_server"))
     }
 }
--- Frontend/Assets/Scripts/NativePluginTest.cs
+++ Frontend/Assets/Scripts/NativePluginTest.cs
@@ -8,49 +8,28 @@ using UnityEngine;
 
 public class NativePluginTest : MonoBehaviour
 {
-    [DllImport("bootstrapper")]
-    private static extern int open_server();
-
-    [DllImport("bootstrapper")]
-    private static extern void close_server(int pid);
-
-    [DllImport("bootstrapper")]
-    private static extern int get_tcp_port(int pid);
-
     public TextMeshProUGUI text;
 
-    private IEnumerator Start()
+    void Start()
     {
-        var pid = open_server();
-
-        var port = get_tcp_port(pid);
-
-        Debug.Log(port);
-
-        var tcpClient = new TcpClient();
-        tcpClient.Connect(IPAddress.Loopback, port);
-        var stream = tcpClient.GetStream();
-
-        var encoding = new ASCIIEncoding();
-        var command = encoding.GetBytes("\"go\"");
-
-        var buffer = new byte[1];
-
-        for (int _ = 0; _ < 10; _++)
+        using (var server = new AiServer())
         {
+            var tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Loopback, server.Port);
+            var stream = tcpClient.GetStream();
+
+            var encoding = new ASCIIEncoding();
+            var command = encoding.GetBytes("\"go\"");
+
+            var buffer = new byte[1];
+
             stream.Write(command);
             stream.Read(buffer);
             var response = buffer[0];
 
             text.text = response.ToString();
-            yield return new WaitForSeconds(1);
 
-            text.text = "";
-            yield return new WaitForSeconds(1);
+            server.IntentionallyError();
         }
-
-        text.text = "done";
-
-        close_server(pid);
     }
 }
