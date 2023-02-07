bd3e8f8a4f035abc344133c27fee8ab8699d737b
Chris Janidlo <christopherjanidlo@gmail.com>
2023-02-06 22:50:54 -0600

    Fix board coordinate order, add tests
    Previously, each row was associated with the X axis, and each column the
    Y axis. While that was consistent and it worked, it was 1) unintuitive
    and 2) unintentional, so I've swapped them around. To ensure they're
    always in agreement, I've added tests.

--- AI/crates/game_rules/src/deacon/board.rs
+++ AI/crates/game_rules/src/deacon/board.rs
@@ -1,5 +1,7 @@
 use std::ops::Mul;
 
+use itertools::*;
+
 pub use super::{form::Form, Piece, Player};
 
 macro_rules! board {
@@ -89,20 +91,21 @@ impl Default for Board {
 
 impl Board {
     pub fn get(&self, coord: Coordinate) -> &Option<Piece> {
-        &self.positions[coord.x as usize][coord.y as usize]
+        &self.positions[coord.y as usize][coord.x as usize]
     }
 
     pub fn set(&mut self, coord: Coordinate, value: Option<Piece>) {
-        self.positions[coord.x as usize][coord.y as usize] = value;
+        self.positions[coord.y as usize][coord.x as usize] = value;
     }
 
     pub fn enumer_coords(&self) -> impl Iterator<Item = (Coordinate, &Option<Piece>)> {
-        self.positions.iter().enumerate().flat_map(|(x, row)| {
-            row.iter().enumerate().map(move |(y, p)| {
-                let x = x.try_into().unwrap();
-                let y = y.try_into().unwrap();
-                (Coordinate { x, y }, p)
-            })
+        iproduct!(0..BOARD_LENGTH, 0..BOARD_LENGTH).map(|(x, y)| {
+            let x = x.try_into().unwrap();
+            let y = y.try_into().unwrap();
+            let coord = Coordinate { x, y };
+            let piece = self.get(coord);
+
+            (coord, piece)
         })
     }
 
@@ -190,6 +193,8 @@ impl Mul<i8> for RelCoord {
 
 #[cfg(test)]
 mod tests {
+    use pretty_assertions::assert_eq;
+    use rand::prelude::*;
     use rstest::*;
 
     use super::*;
@@ -275,5 +280,251 @@ mod tests {
         };
 
         assert_eq!(board_macro, board_struct_expression);
+    }
+
+    #[rstest]
+    #[case::empty_board(
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+        ],
+        vec![
+            (coord_unwrapped(0, 0), None::<Piece>),
+            (coord_unwrapped(0, 1), None::<Piece>),
+            (coord_unwrapped(0, 2), None::<Piece>),
+            (coord_unwrapped(0, 3), None::<Piece>),
+            (coord_unwrapped(0, 4), None::<Piece>),
+            (coord_unwrapped(1, 0), None::<Piece>),
+            (coord_unwrapped(1, 1), None::<Piece>),
+            (coord_unwrapped(1, 2), None::<Piece>),
+            (coord_unwrapped(1, 3), None::<Piece>),
+            (coord_unwrapped(1, 4), None::<Piece>),
+            (coord_unwrapped(2, 0), None::<Piece>),
+            (coord_unwrapped(2, 1), None::<Piece>),
+            (coord_unwrapped(2, 2), None::<Piece>),
+            (coord_unwrapped(2, 3), None::<Piece>),
+            (coord_unwrapped(2, 4), None::<Piece>),
+            (coord_unwrapped(3, 0), None::<Piece>),
+            (coord_unwrapped(3, 1), None::<Piece>),
+            (coord_unwrapped(3, 2), None::<Piece>),
+            (coord_unwrapped(3, 3), None::<Piece>),
+            (coord_unwrapped(3, 4), None::<Piece>),
+            (coord_unwrapped(4, 0), None::<Piece>),
+            (coord_unwrapped(4, 1), None::<Piece>),
+            (coord_unwrapped(4, 2), None::<Piece>),
+            (coord_unwrapped(4, 3), None::<Piece>),
+            (coord_unwrapped(4, 4), None::<Piece>),
+        ]
+    )]
+    #[case::coordinate_ordering(
+        board![
+            [_, S, _, _, _];
+            [_, _, _, _, _];
+            [S, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+        ],
+        vec![
+            (coord_unwrapped(0, 0), None::<Piece>),
+            (coord_unwrapped(0, 1), None::<Piece>),
+            (coord_unwrapped(0, 2), Some(Piece { form: Form::Scientist, owner: Player::Blue })),
+            (coord_unwrapped(0, 3), None::<Piece>),
+            (coord_unwrapped(0, 4), None::<Piece>),
+            (coord_unwrapped(1, 0), Some(Piece { form: Form::Scientist, owner: Player::Blue })),
+            (coord_unwrapped(1, 1), None::<Piece>),
+            (coord_unwrapped(1, 2), None::<Piece>),
+            (coord_unwrapped(1, 3), None::<Piece>),
+            (coord_unwrapped(1, 4), None::<Piece>),
+            (coord_unwrapped(2, 0), None::<Piece>),
+            (coord_unwrapped(2, 1), None::<Piece>),
+            (coord_unwrapped(2, 2), None::<Piece>),
+            (coord_unwrapped(2, 3), None::<Piece>),
+            (coord_unwrapped(2, 4), None::<Piece>),
+            (coord_unwrapped(3, 0), None::<Piece>),
+            (coord_unwrapped(3, 1), None::<Piece>),
+            (coord_unwrapped(3, 2), None::<Piece>),
+            (coord_unwrapped(3, 3), None::<Piece>),
+            (coord_unwrapped(3, 4), None::<Piece>),
+            (coord_unwrapped(4, 0), None::<Piece>),
+            (coord_unwrapped(4, 1), None::<Piece>),
+            (coord_unwrapped(4, 2), None::<Piece>),
+            (coord_unwrapped(4, 3), None::<Piece>),
+            (coord_unwrapped(4, 4), None::<Piece>),
+        ]
+    )]
+    fn test_enumer_coords(
+        #[case] board: Board,
+        #[case] expected_enumeration: Vec<(Coordinate, Option<Piece>)>,
+    ) {
+        let actual_enumeration = board
+            .enumer_coords()
+            .map(|(c, p)| (c, p.to_owned()))
+            .collect_vec();
+
+        assert_eq!(actual_enumeration, expected_enumeration);
+    }
+
+    #[rstest]
+    #[case::empty(
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+        ],
+        coord_unwrapped(0, 0),
+        None
+    )]
+    #[case::non_empty(
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, E, _, _, _];
+            [_, _, _, _, _];
+        ],
+        coord_unwrapped(1, 3),
+        Some(Piece { form: Form::Engineer, owner: Player::Blue })
+    )]
+    fn test_get_position(
+        #[case] board: Board,
+        #[case] coord: Coordinate,
+        #[case] expected_piece: Option<Piece>,
+    ) {
+        let actual_piece = board.get(coord).to_owned();
+
+        assert_eq!(actual_piece, expected_piece);
+    }
+
+    #[rstest]
+    #[case::last_corner_from_empty(
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+        ],
+        coord_unwrapped(4, 4),
+        Some(Piece { form: Form::Pilot, owner: Player::Red }),
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, i];
+        ],
+    )]
+    #[case::proper_ordering(
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+        ],
+        coord_unwrapped(2, 3),
+        Some(Piece { form: Form::Priest, owner: Player::Red }),
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, p, _, _];
+            [_, _, _, _, _];
+        ],
+    )]
+    #[case::overwrite(
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, P, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+        ],
+        coord_unwrapped(2, 2),
+        Some(Piece { form: Form::Captain, owner: Player::Red }),
+        board![
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+            [_, _, c, _, _];
+            [_, _, _, _, _];
+            [_, _, _, _, _];
+        ],
+    )]
+    fn test_set_position(
+        #[case] mut start_board: Board,
+        #[case] coord: Coordinate,
+        #[case] piece: Option<Piece>,
+        #[case] expected_board: Board,
+    ) {
+        start_board.set(coord, piece);
+
+        assert_eq!(start_board, expected_board);
+    }
+
+    #[rstest]
+    fn test_set_position_get_position_parity() {
+        fn random_board() -> Board {
+            let mut board = Board::default();
+
+            for (x, y) in iproduct!(0..BOARD_LENGTH, 0..BOARD_LENGTH) {
+                board.positions[x][y] = random_piece();
+            }
+
+            board
+        }
+
+        fn random_piece() -> Option<Piece> {
+            let r: u8 = rand::random();
+
+            if r > 127 {
+                None
+            } else {
+                let owner = if r % 2 == 0 {
+                    Player::Blue
+                } else {
+                    Player::Red
+                };
+
+                let form = match r {
+                    // 128 / 6 ~= 21
+                    _ if r < 21 => Form::Captain,
+                    _ if r < 42 => Form::Engineer,
+                    _ if r < 63 => Form::Pilot,
+                    _ if r < 84 => Form::Priest,
+                    _ if r < 105 => Form::Robot,
+                    _ => Form::Scientist,
+                };
+
+                Some(Piece { form, owner })
+            }
+        }
+
+        fn random_coord() -> Coordinate {
+            let x = rand::thread_rng().gen_range(0..BOARD_LENGTH);
+            let y = rand::thread_rng().gen_range(0..BOARD_LENGTH);
+
+            coord_unwrapped(x as u8, y as u8)
+        }
+
+        for _ in 0..100 {
+            let start_board: Board = random_board();
+            let piece: Option<Piece> = random_piece();
+            let coord: Coordinate = random_coord();
+
+            let mut new_board = start_board.clone();
+            new_board.set(coord, piece.clone());
+
+            let result = new_board.get(coord).to_owned();
+
+            assert_eq!(
+                piece, result,
+                "\n\t* start_board: {start_board:?}\n\t* coord:{coord:?}\n\t* new_board: {new_board:?}"
+            );
+        }
     }
 }
