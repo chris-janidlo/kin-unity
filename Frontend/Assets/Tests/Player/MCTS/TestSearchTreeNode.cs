75eae04876c5b15f8c74ea82467b05e1dd1c1d00
Chris Janidlo <christopherjanidlo@gmail.com>
2023-05-07 22:31:24 -0500

    Add SearchTreeNode test structure and BackUp tests

---
+++ Frontend/Assets/Tests/Player/MCTS/TestSearchTreeNode.cs
@@ -0,0 +1,68 @@
+using Code.Player.MCTS;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.Player.MCTS
+{
+    using ConcreteNode = SearchTreeNode<TestPlayer, TestState, TestAction>;
+    using ConcreteChild = ChildSearchTreeNode<TestPlayer, TestState, TestAction>;
+
+    public class TestSearchTreeNode
+    {
+        private static ConcreteNode RandomRoot()
+        {
+            var state = new TestState(Random.Range(0, 10));
+
+            return new ConcreteNode(state);
+        }
+
+        private static ConcreteChild RandomChild(ConcreteNode parent)
+        {
+            var state = new TestState(Random.Range(0, 10));
+            var action = new TestAction(state.Value - parent.GameState.Value);
+
+            return new ConcreteChild(state, parent, action);
+        }
+
+        [Test]
+        public void BackupBacksUp()
+        {
+            var node1 = RandomRoot();
+            var node2 = RandomChild(node1);
+            var node3 = RandomChild(node2);
+            var node4 = RandomChild(node3);
+
+            float score = Random.value;
+            node4.Backup(score);
+
+            Assert.AreEqual(score, node4.Score);
+            Assert.AreEqual(-score, node3.Score);
+            Assert.AreEqual(score, node2.Score);
+            Assert.AreEqual(-score, node1.Score);
+        }
+
+        [Test]
+        public void BackupAvoidsUnrelatedNodes()
+        {
+            var node1 = RandomRoot();
+            var node2 = RandomChild(node1);
+            var node3 = RandomChild(node2);
+            var node4 = RandomChild(node3);
+
+            var nodeA = RandomChild(node1);
+            var nodeA1 = RandomChild(nodeA);
+            var nodeB = RandomChild(node2);
+            var nodeC = RandomChild(node3);
+            var nodeD = RandomChild(node4);
+
+            float score = Random.value;
+            node4.Backup(score);
+
+            Assert.AreEqual(0, nodeA.Score);
+            Assert.AreEqual(0, nodeA1.Score);
+            Assert.AreEqual(0, nodeB.Score);
+            Assert.AreEqual(0, nodeC.Score);
+            Assert.AreEqual(0, nodeD.Score);
+        }
+    }
+}
