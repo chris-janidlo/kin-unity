552a6ff562169964fc913eba5a73f7aeaeca6edb
Chris Janidlo <christopherjanidlo@gmail.com>
2023-05-08 18:56:12 -0500

    Move child-specific tests to a child class
    Also use only the new constraint-style tests

--- Frontend/Assets/Tests/Player/MCTS/TestSearchTreeNode.cs
+++ Frontend/Assets/Tests/Player/MCTS/TestSearchTreeNode.cs
@@ -8,68 +8,30 @@ namespace Tests.Player.MCTS
     using ConcreteNode = SearchTreeNode<TestPlayer, TestState, TestAction>;
     using ConcreteChild = ChildSearchTreeNode<TestPlayer, TestState, TestAction>;
 
-    public class TestSearchTreeNode
+    internal class Helper
     {
-        private static ConcreteNode RandomRoot()
+        internal static ConcreteNode RandomRoot()
         {
             var state = new TestState(Random.Range(0, 10));
 
             return new ConcreteNode(state);
         }
 
-        private static ConcreteChild RandomChild(ConcreteNode parent)
+        internal static ConcreteChild RandomChild(ConcreteNode parent)
         {
             var state = new TestState(Random.Range(0, 10));
             var action = new TestAction(state.Value - parent.GameState.Value);
 
             return new ConcreteChild(state, parent, action);
         }
+    }
 
-        [Test]
-        public void BackupBacksUp()
-        {
-            var node1 = RandomRoot();
-            var node2 = RandomChild(node1);
-            var node3 = RandomChild(node2);
-            var node4 = RandomChild(node3);
-
-            float score = Random.value;
-            node4.Backup(score);
-
-            Assert.AreEqual(score, node4.Score);
-            Assert.AreEqual(-score, node3.Score);
-            Assert.AreEqual(score, node2.Score);
-            Assert.AreEqual(-score, node1.Score);
-        }
-
-        [Test]
-        public void BackupAvoidsUnrelatedNodes()
-        {
-            var node1 = RandomRoot();
-            var node2 = RandomChild(node1);
-            var node3 = RandomChild(node2);
-            var node4 = RandomChild(node3);
-
-            var nodeA = RandomChild(node1);
-            var nodeA1 = RandomChild(nodeA);
-            var nodeB = RandomChild(node2);
-            var nodeC = RandomChild(node3);
-            var nodeD = RandomChild(node4);
-
-            float score = Random.value;
-            node4.Backup(score);
-
-            Assert.AreEqual(0, nodeA.Score);
-            Assert.AreEqual(0, nodeA1.Score);
-            Assert.AreEqual(0, nodeB.Score);
-            Assert.AreEqual(0, nodeC.Score);
-            Assert.AreEqual(0, nodeD.Score);
-        }
-
+    public class TestSearchTreeNode
+    {
         [Test]
         public void ExpandAddsNewNodeAsChildOfOriginal()
         {
-            var parent = RandomRoot();
+            var parent = Helper.RandomRoot();
             var expanded = parent.Expand();
 
             Assert.That(parent.Children, Has.Member(expanded));
@@ -79,8 +41,8 @@ namespace Tests.Player.MCTS
         [Test]
         public void ExpandFindsMoveToExpandOn()
         {
-            var parent = RandomRoot();
-            var node = RandomChild(parent);
+            var parent = Helper.RandomRoot();
+            var node = Helper.RandomChild(parent);
 
             var expanded = node.Expand();
 
@@ -90,7 +52,7 @@ namespace Tests.Player.MCTS
         [Test]
         public void ExpandConsumesUnvisitedActions()
         {
-            var node = RandomRoot();
+            var node = Helper.RandomRoot();
 
             var originalUnvisitedActions = new HashSet<TestAction>(node.UnvisitedActions);
             int lastCount = node.UnvisitedActions.Count;
@@ -108,6 +70,50 @@ namespace Tests.Player.MCTS
 
                 lastCount = newCount;
             }
+        }
+    }
+
+    public class TestChildSearchTreeNode
+    {
+        [Test]
+        public void BackupBacksUp()
+        {
+            var node1 = Helper.RandomRoot();
+            var node2 = Helper.RandomChild(node1);
+            var node3 = Helper.RandomChild(node2);
+            var node4 = Helper.RandomChild(node3);
+
+            float score = Random.value;
+            node4.Backup(score);
+
+            Assert.That(node4.Score, Is.EqualTo(score));
+            Assert.That(node3.Score, Is.EqualTo(-score));
+            Assert.That(node2.Score, Is.EqualTo(score));
+            Assert.That(node1.Score, Is.EqualTo(-score));
+        }
+
+        [Test]
+        public void BackupAvoidsUnrelatedNodes()
+        {
+            var node1 = Helper.RandomRoot();
+            var node2 = Helper.RandomChild(node1);
+            var node3 = Helper.RandomChild(node2);
+            var node4 = Helper.RandomChild(node3);
+
+            var nodeA = Helper.RandomChild(node1);
+            var nodeA1 = Helper.RandomChild(nodeA);
+            var nodeB = Helper.RandomChild(node2);
+            var nodeC = Helper.RandomChild(node3);
+            var nodeD = Helper.RandomChild(node4);
+
+            float score = Random.value;
+            node4.Backup(score);
+
+            Assert.That(nodeA.Score, Is.EqualTo(0));
+            Assert.That(nodeA1.Score, Is.EqualTo(0));
+            Assert.That(nodeB.Score, Is.EqualTo(0));
+            Assert.That(nodeC.Score, Is.EqualTo(0));
+            Assert.That(nodeD.Score, Is.EqualTo(0));
         }
     }
 }
